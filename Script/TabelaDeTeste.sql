e979c5a931f7e8e1e7b18ef4e9d75fe750b72df2
Heitor <heitorccvp@gmail.com>
2019-07-19 02:44:11 -0300

    Create TabelaDeTeste.sql
    Script da tabela de teste

---
+++ Script/TabelaDeTeste.sql
@@ -0,0 +1,25 @@
+USE [TESTE]
+GO
+
+/****** Object:  Table [dbo].[Teste]    Script Date: 19/07/2019 02:43:17 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Teste](
+	[1] [nchar](10) NULL,
+	[2] [nchar](10) NULL,
+	[3] [nchar](10) NULL,
+	[4] [nchar](10) NULL,
+	[5] [nchar](10) NULL,
+	[6] [nchar](10) NULL,
+	[7] [nchar](10) NULL,
+	[8] [nchar](10) NULL,
+	[9] [nchar](10) NULL,
+	[10] [nchar](10) NULL
+) ON [PRIMARY]
+GO
+
+
